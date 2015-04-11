924d1dd4fe53f0865982bbf8eafeeb8db55a3762
Titus Woo <titus.woo@me.com>
2015-04-11 02:17:24 -0400

    improved profile page.

--- app/profile.php
+++ app/profile.php
@@ -3,60 +3,61 @@ include_once("../resources/Error.php");
 include_once("../resources/Validator.php");
 include_once("../resources/User.php");
 $validator = new Validator();
+
 ?>
 <?php require_once("../resources/templates/header.php"); ?>
-<div class="ui tall stacked segment">
-    <h1>Your Profile</h1>
+    <div class="ui tall stacked segment">
+        <h1>Your Profile</h1>
 
-    <form class="ui form" action="search-books.php" method="post">
-        <div class="field">
-            <label>First Name</label>
-            <input type="text" name="first_name"/>
-        </div>
-        <div class="field">
-            <label>Last Name</label>
-            <input type="text" name="last_name"/>
-        </div>
-        <div class="field">
-            <label>DOB</label>
-            <input type="text" name="dob"/>
-        </div>
-        <div class="field">
-            <label>Gender</label>
-            <select class="ui dropdown">
-                <option value="">Gender</option>
-                <option value="1">Male</option>
-                <option value="0">Female</option>
-            </select>
-        </div>
-        <div class="field">
-            <label>Email</label>
-            <input type="text" name="email"/>
-        </div>
-        <div class="inline field">
-            <div class="ui checkbox">
-                <input type="checkbox">
-                <label>Faculty member</label>
+        <form class="ui form" action="profile.php" method="post">
+            <div class="field">
+                <label>First Name</label>
+                <input type="text" name="first_name"/>
             </div>
-        </div>
-        <div class="field">
-            <label>Address</label>
-            <input type="text" name="address"/>
-        </div>
-        <div class="field">
-            <label>Associated Department</label>
-            <select class="ui dropdown">
-                <option value="">Associated Department</option>
-            </select>
-        </div>
-    </form>
-    <a href="search-books.php" class="ui left labeled icon button">
-        <i class="left arrow icon"></i>
-        Back
-    </a>
-    <button class="ui primary button right floated">
-        <i class="save icon"></i>
-        Save
-    </button>
-</div>
+            <div class="field">
+                <label>Last Name</label>
+                <input type="text" name="last_name"/>
+            </div>
+            <div class="field">
+                <label>DOB</label>
+                <input type="text" name="dob"/>
+            </div>
+            <div class="field">
+                <label>Gender</label>
+                <select class="ui dropdown">
+                    <option value="">Gender</option>
+                    <option value="1">Male</option>
+                    <option value="0">Female</option>
+                </select>
+            </div>
+            <div class="field">
+                <label>Email</label>
+                <input type="email" name="email"/>
+            </div>
+            <div class="field">
+                <label>Address</label>
+                <input type="text" name="address"/>
+            </div>
+            <div class="inline field">
+                <div class="ui checkbox">
+                    <input type="checkbox">
+                    <label>Faculty member</label>
+                </div>
+            </div>
+            <div class="field">
+                <label>Associated Department</label>
+                <select class="ui dropdown">
+                    <option value="">Associated Department</option>
+                </select>
+            </div>
+            <a href="search-books.php" class="ui left labeled icon button">
+                <i class="left arrow icon"></i>
+                Back
+            </a>
+            <button type="submit" class="ui primary button right floated">
+                <i class="save icon"></i>
+                Save
+            </button>
+        </form>
+    </div>
 <?php require_once("../resources/templates/footer.php"); ?>
