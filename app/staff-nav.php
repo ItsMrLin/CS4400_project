af93610432b7bdf6b0f63110790b3128c56419dd
Titus Woo <titus.woo@me.com>
2015-04-21 22:30:28 -0400

    More consistent ui for staff view actions.

--- app/staff-nav.php
+++ app/staff-nav.php
@@ -8,13 +8,15 @@ if (!$user->isStaff()) {
 require("../resources/templates/header.php");
 ?>
 
-<h1>
-    Staff View
-</h1>
-<div class="ui list centered">
-  <div class="item"><a href="report-damage.php">Report Damage/Lost Book</a></div>
-  <div class="item"><a href="popular-book-report.php">Popular Book Report</a></div>
-  <div class="item"><a href="popular-subject-report.php">Popular Subject Report</a></div>
-  <div class="item"><a href="frequent-user-report.php">Frequent User Report</a></div>
-  <div class="item"><a href="damaged-book-report.php">Damaged Books Report</a></div>
+
+<div class="ui tall stacked segment">
+    <h1>Staff View</h1>
+
+    <div class="ui list centered">
+        <div class="item"><a href="report-damage.php">Report Damage/Lost Book</a></div>
+        <div class="item"><a href="popular-book-report.php">Popular Book Report</a></div>
+        <div class="item"><a href="popular-subject-report.php">Popular Subject Report</a></div>
+        <div class="item"><a href="frequent-user-report.php">Frequent User Report</a></div>
+        <div class="item"><a href="damaged-book-report.php">Damaged Books Report</a></div>
+    </div>
 </div>
