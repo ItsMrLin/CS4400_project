14ce1c158189654c2a9e66296bf66c2ff3832e4e
Titus Woo <titus.woo@me.com>
2015-04-22 01:07:08 -0400

    Request extension page almost working... one little bug with one of the sql queries.

--- app/request-extension.php
+++ app/request-extension.php
@@ -15,53 +15,56 @@ $form->onSubmit(function ($props) use ($form, $validator, &$resultData, $user) {
     if ($validator->valid()) {
         // if he checked out the book from the library:
 
-        $isbn = $props['$ISBN'];
+        $isbn = $props['ISBN'];
         $username = $user->getUsername();
 
         $mysqli = require("../resources/db_connection.php");
-        $query = "SELECT CopyID FROM BookCopy
-                  WHERE ISBN=$isbn AND FutureRequester=$username";
+        $query = "SELECT * FROM Issue
+                  WHERE ISBN=$isbn
+                  AND ReturnDate>CURDATE()
+                  AND Username='$username'";
 
         $results = $mysqli->query($query);
 
         if ($results->num_rows > 0) {
-            echo "can";
+            // Then the guy has this book checked out already.
+            $r = $results->fetch_array(MYSQLI_ASSOC);
+            print_r($r);
+            $extensionDate = $r['ExtensionDate'];
+
+            // set extension limit based on user type:
+
+            if ($user->isStaff()) {
+                // then this dude is a staff member
+                $extensionLimit = 5;
+            } else {
+                $extensionLimit = 2; // defaults to non-staff
+            }
+
+            if ($r['ExtensionCount'] <= $extensionLimit) {
+                if ($extensionDate == "") {
+                    echo "HERE";
+                    $query = "UPDATE Issue
+                          SET ExtensionDate=CURDATE(), ExtensionCount=ExtensionCount+1, ReturnDate=(CURDATE()+14)
+                          WHERE Username='$username'
+                          AND ISBN=$isbn
+                          AND ReturnDate>CURDATE()";
+                } else {
+                    echo "ELSEWHERE";
+                    $query = "UPDATE Issue
+                          SET ExtensionCount=ExtensionCount+1, ReturnDate=(ExtensionDate+14)
+                          WHERE Username='$username'
+                          AND ISBN=$isbn
+                          AND ReturnDate>CURDATE()";
+                }
+
+                $mysqli->query($query);
+            } else {
+                $validator->add(new Error('1', "You can't request an extesnion for this book more than $extensionLimit times."));
+            }
         } else {
-            $validator->add(new Error('top', "You can't request an extension for this book."));
+            $validator->add(new Error('1', "You can't request an extension for this book at this time."));
         }
-
-        /*$isbn = $props['ISBN'];
-        $mysqli = require("../resources/db_connection.php");
-        $query = "SELECT b.ISBN, bc.CopyID, MIN(i.ReturnDate) AS 'ExpectedAvailableDate'
-                  FROM Book AS b
-                  INNER JOIN BookCopy AS bc
-                  ON b.ISBN=bc.ISBN
-                  INNER JOIN Issue AS i
-                  ON i.ISBN=b.ISBN
-                  WHERE b.ISBN=$isbn
-                  GROUP BY 'ExpectedAvailableDate'
-                  AND 'ExpectedAvailableDate' IS NOT NULL";
-
-        $results = $mysqli->query($query);
-        $resultData = $results->fetch_array(MYSQLI_ASSOC);
-
-        if ($results->num_rows > 0) {
-
-            // so someone else already extended it.
-            $isbn = $resultData['ISBN'];
-            $CopyID = $resultData['CopyID'];
-            $ExpectedAvailableDate = $resultData['ExpectedAvailableDate'];
-            $username = $user->getUsername();
-
-            $query = "UPDATE BookCopy bc
-                      SET bc.FutureRequester=$username
-                      WHERE bc.ISBN=$isbn AND bc.CopyID=$CopyID";
-            $result = $mysqli->query($query);
-        } else {
-            $validator->add(new Error("top", "A request extension for this book cannot be issued!"));
-        }
-
-        print_r($resultData);*/
     }
 });
 
